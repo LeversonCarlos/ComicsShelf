77f6e3fa929b589d6dbd07a36fc95c8ab787b369
Leverson Carlos <lcjohnny@hotmail.com>
2018-04-11 11:54:54 -0300

    Android Icon

--- ComicsShelf.Android/MainActivity.cs
+++ ComicsShelf.Android/MainActivity.cs
@@ -1,15 +1,10 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace ComicsShelf.Droid
 {
-   [Activity(Label = "ComicsShelf", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+   [Activity(Label = "Comics Shelf", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
    {
       protected override void OnCreate(Bundle bundle)
