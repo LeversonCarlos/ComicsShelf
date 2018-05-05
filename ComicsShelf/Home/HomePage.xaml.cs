aa989d0c01e83049a1840ea01e3db4ff7d55e464
Leverson Carlos <lcjohnny@hotmail.com>
2018-05-05 14:35:53 -0300

    Split the home screen into tabs

--- ComicsShelf/Home/HomePage.xaml.cs
+++ ComicsShelf/Home/HomePage.xaml.cs
@@ -1,13 +1,36 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace ComicsShelf.Home
 {
    [XamlCompilation(XamlCompilationOptions.Compile)]
-   public partial class HomePage : Helpers.Controls.Page
+   public partial class HomePage : TabbedPage
    {
+
       public HomePage()
       {
          InitializeComponent();
+         if (Device.RuntimePlatform == Device.iOS)
+         { this.Padding = new Thickness(0, 20, 0, 0); }
       }
+
+      protected override void OnAppearing()
+      {
+         base.OnAppearing();
+         if (this.BindingContext == null) { return; }
+         // this.Children[0].BindingContext = this.BindingContext;
+         // this.Children[1].BindingContext = this.BindingContext;
+         // this.Children[2].BindingContext = this.BindingContext;
+         this.SetBinding(Page.TitleProperty, "Title");
+         (this.BindingContext as Helpers.ViewModels.BaseVM).InitializeAsync();
+      }
+
+      protected override void OnDisappearing()
+      {
+         base.OnDisappearing();
+         if (this.BindingContext == null) { return; }
+         (this.BindingContext as Helpers.ViewModels.BaseVM).FinalizeAsync();
+      }
+
    }
 }
---
+++ ComicsShelf/Home/ToolsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ComicsShelf.Home
+{
+   [XamlCompilation(XamlCompilationOptions.Compile)]
+   public partial class ToolsPage : ContentPage
+   {
+      public ToolsPage()
+      {
+         InitializeComponent();
+         this.Title = R.Strings.HOME_TOOLS_PAGE_TITLE;
+      }
+   }
+}
