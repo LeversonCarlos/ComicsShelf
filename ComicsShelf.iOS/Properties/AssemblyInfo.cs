b41a8974c2c2dfde8981844c5141a0a9a9ada4e6
Leverson Carlos <lcjohnny@hotmail.com>
2018-05-20 23:23:06 -0300

    Version

--- ComicsShelf.iOS/Properties/AssemblyInfo.cs
+++ ComicsShelf.iOS/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.05.06.0")]
-[assembly: AssemblyFileVersion("18.05.06.0")]
+[assembly: AssemblyVersion("18.05.07.0")]
+[assembly: AssemblyFileVersion("18.05.07.0")]
