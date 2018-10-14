df774516340f5c81a2a0aa6fbf2345dae1478bd9
Leverson Carlos <lcjohnny@hotmail.com>
2018-10-14 15:05:15 -0300

    version

--- ComicsShelf.Android/Properties/AssemblyInfo.cs
+++ ComicsShelf.Android/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.10.9.0")]
-[assembly: AssemblyFileVersion("18.10.9.0")]
+[assembly: AssemblyVersion("18.10.11.0")]
+[assembly: AssemblyFileVersion("18.10.11.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
