3875a04cb6bd5b3fe8ed9893a0f262c1953a4a90
Leverson Carlos <lcjohnny@hotmail.com>
2018-10-21 15:14:02 -0300

    regions

--- ComicsShelf/Helpers/Controls/Controls/PageStatsView.cs
+++ ComicsShelf/Helpers/Controls/Controls/PageStatsView.cs
@@ -64,5 +64,9 @@ namespace ComicsShelf.Helpers.Controls
       { (bindable as PageStatsView).readingProgress.Progress = (double)newValue; }
       #endregion
 
+      #region ShowStats
+      
+      #endregion
+
    }
 }
