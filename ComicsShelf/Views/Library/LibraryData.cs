c4ff4e9ca2e6aa368c9f0c4cdaefb2b6532879c6
Leverson Carlos <lcjohnny@hotmail.com>
2018-11-15 14:48:05 -0200

    library list

--- ComicsShelf/Views/Library/LibraryData.cs
+++ ComicsShelf/Views/Library/LibraryData.cs
@@ -1,7 +1,37 @@
-﻿namespace ComicsShelf.Views.Library
+﻿using Xamarin.Forms.Internals;
+
+namespace ComicsShelf.Views.Library
 {
    public class LibraryData : Helpers.Observables.ObservableObject
    {
+
+      #region New
+      public LibraryData()
+      {
+         this.Libraries = new Helpers.Observables.ObservableList<LibraryDataItem>();
+         this.Libraries.CollectionChanged += this.Libraries_CollectionChanged;
+
+         var libraries = App.Database.Table<Helpers.Database.Library>();
+         libraries.ForEach(library => this.Libraries.Add(new LibraryDataItem(library)));
+      }
+      #endregion
+
+      #region Libraries
+
+      public Helpers.Observables.ObservableList<LibraryDataItem> Libraries { get; set; }
+
+      bool _HasLibraries;
+      public bool HasLibraries
+      {
+         get { return this._HasLibraries; }
+         set { this.SetProperty(ref this._HasLibraries, value); }
+      }
+
+      private void Libraries_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+      { this.HasLibraries = this.Libraries.Count != 0; }
+
+      #endregion
+
 
       #region LibraryPath
       string _LibraryPath;
@@ -34,6 +64,7 @@
          set { this.SetProperty(ref this._IsLibraryDefined, value); }
       }
       #endregion
+
 
    }
 }
