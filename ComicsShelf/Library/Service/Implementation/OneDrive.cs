1579c62fceadda1acdb6a3deb4dca62040fa9681
Leverson Carlos <lcjohnny@hotmail.com>
2019-01-02 15:59:12 -0200

    xamarin onedrive service

--- ComicsShelf/Library/Service/Implementation/OneDrive.cs
+++ ComicsShelf/Library/Service/Implementation/OneDrive.cs
@@ -1,26 +1,72 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Xamarin.OneDrive.Profile;
+using Xamarin.OneDrive.Files;
+using System.Linq;
+using System;
 
 namespace ComicsShelf.Library.Implementation
 {
    internal class OneDrive : ILibraryService
    {
 
-      public async Task<bool> Validate(Helpers.Database.Library library)
+      Xamarin.OneDrive.Connector Connector { get; set; }
+      public OneDrive()
       {
-         return true;
+         var clientID = System.Environment.GetEnvironmentVariable("clientID");
+         this.Connector = new Xamarin.OneDrive.Connector(clientID, "User.Read", "Files.Read");
       }
+
+      public async Task<bool> Validate(Helpers.Database.Library library)
+      { return await this.Connector.ConnectAsync(); }
 
       public async Task<bool> AddLibrary(Helpers.Database.Library library)
       {
-         library.LibraryPath = "OneDrive";
-         library.LibraryText = "OneDrive";
+         if (!await this.Connector.ConnectAsync()) { return false; }
+         var profile = await this.Connector.GetProfileAsync();
+         if (profile == null) { return false; }
+         library.LibraryPath = profile.id;
+         library.LibraryText = profile.Name;
          return true;
       }
 
       public async Task<List<Helpers.Database.ComicFile>> SearchFilesAsync(Helpers.Database.Library library)
       {
-         return null;
+         try
+         {
+
+            // LOCATE FILES
+            var fileList = await this.Connector.SearchFilesAsync("*.cbz");
+
+            // CONVERT
+            var comicFiles = fileList
+               .Select(file => new Helpers.Database.ComicFile
+               {
+                  LibraryPath = library.LibraryPath,
+                  Key = file.id,
+                  FullPath = $"{file.FilePath}/{file.FileName}",
+                  ParentPath = file.FilePath,
+                  ReleaseDate = (!file.CreatedDateTime.HasValue ? "" : App.Database.GetDate(file.CreatedDateTime.Value.ToLocalTime())),
+                  Available = true
+               })
+               .ToList();
+
+            // TEXT
+            foreach (var comicFile in comicFiles)
+            {
+               comicFile.FullText = System.IO.Path.GetFileNameWithoutExtension(comicFile.FullPath).Trim();
+               var folderText = System.IO.Path.GetFileNameWithoutExtension(comicFile.ParentPath);
+               if (!string.IsNullOrEmpty(folderText))
+               { comicFile.SmallText = comicFile.FullText.Replace(folderText, ""); }
+               if (string.IsNullOrEmpty(comicFile.SmallText))
+               { comicFile.SmallText = comicFile.FullText; }
+            }
+
+            // RESULT
+            return comicFiles;
+
+         }
+         catch (Exception) { throw; }
       }
 
       public async Task ExtractCoverAsync(Helpers.Database.Library library, Helpers.Database.ComicFile comicFile)
