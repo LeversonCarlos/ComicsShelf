c85995163e62359d5ef3b5eb0734c91997045c9e
Leverson Carlos <lcjohnny@hotmail.com>
2019-01-04 16:57:33 -0200

    library service searchFiles

---
+++ ComicsShelf/Library/Service/FileSystem/FileSystem.SearchFilesAsync.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicsShelf.Library.Implementation
+{
+   partial class FileSystemService
+   {
+
+      public async Task<List<Helpers.Database.ComicFile>> SearchFilesAsync(Helpers.Database.Library library)
+      {
+         try
+         {
+
+            // LOCATE FILES
+            var fileList = await this.FileSystem.GetFiles(library.LibraryPath);
+
+            // CONVERT
+            var comicFiles = fileList
+               .Select(file => new Helpers.Database.ComicFile
+               {
+                  LibraryPath = library.LibraryPath,
+                  FullPath = file,
+                  Available = true
+               })
+               .ToList();
+
+            // OTHER PROPERTIES
+            foreach (var comicFile in comicFiles)
+            {
+
+               // PARENT PATH
+               var folderPath = System.IO.Path.GetDirectoryName(comicFile.FullPath);
+               var folderText = System.IO.Path.GetFileNameWithoutExtension(folderPath);
+               comicFile.ParentPath = folderPath;
+
+               // TEXT
+               comicFile.FullText = System.IO.Path.GetFileNameWithoutExtension(comicFile.FullPath).Trim();
+               if (!string.IsNullOrEmpty(folderText))
+               { comicFile.SmallText = comicFile.FullText.Replace(folderText, ""); }
+               if (string.IsNullOrEmpty(comicFile.SmallText))
+               { comicFile.SmallText = comicFile.FullText; }
+
+               // KEY
+               comicFile.Key = comicFile.FullPath
+                  .Replace(App.Settings.Paths.Separator, "_")
+                  .Replace("#", "")
+                  .Replace(".", "_")
+                  .Replace("[", "")
+                  .Replace("]", "")
+                  .Replace("(", "")
+                  .Replace(")", "")
+                  .Replace(" ", "_")
+                  .Replace("___", "_")
+                  .Replace("__", "_");
+
+            }
+
+            // RESULT
+            return comicFiles;
+
+         }
+         catch (Exception) { throw; }
+      }
+
+   }
+}
--- ComicsShelf/Library/Service/OneDrive/OneDrive.cs
+++ ComicsShelf/Library/Service/OneDrive/OneDrive.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.OneDrive.Files;
@@ -14,46 +13,6 @@ namespace ComicsShelf.Library.Implementation
       {
          var clientID = System.Environment.GetEnvironmentVariable("ComicsShelfApplicationID");
          this.Connector = new Xamarin.OneDrive.Connector(clientID, "User.Read", "Files.Read");
-      }
-
-      public async Task<List<Helpers.Database.ComicFile>> SearchFilesAsync(Helpers.Database.Library library)
-      {
-         try
-         {
-
-            // LOCATE FILES
-            var fileList = await this.Connector.SearchFilesAsync("*.cbz");
-
-            // CONVERT
-            var comicFiles = fileList
-               .Select(file => new Helpers.Database.ComicFile
-               {
-                  LibraryPath = library.LibraryPath,
-                  Key = file.id,
-                  FullPath = $"{file.FilePath}/{file.FileName}",
-                  ParentPath = file.FilePath,
-                  ReleaseDate = (!file.CreatedDateTime.HasValue ? "" : App.Database.GetDate(file.CreatedDateTime.Value.ToLocalTime())),
-                  StreamSize = (file.Size.HasValue? file.Size.Value:0),
-                  Available = true
-               })
-               .ToList();
-
-            // TEXT
-            foreach (var comicFile in comicFiles)
-            {
-               comicFile.FullText = System.IO.Path.GetFileNameWithoutExtension(comicFile.FullPath).Trim();
-               var folderText = System.IO.Path.GetFileNameWithoutExtension(comicFile.ParentPath);
-               if (!string.IsNullOrEmpty(folderText))
-               { comicFile.SmallText = comicFile.FullText.Replace(folderText, ""); }
-               if (string.IsNullOrEmpty(comicFile.SmallText))
-               { comicFile.SmallText = comicFile.FullText; }
-            }
-
-            // RESULT
-            return comicFiles;
-
-         }
-         catch (Exception) { throw; }
       }
 
       public async Task ExtractCoverAsync(Helpers.Database.Library library, Helpers.Database.ComicFile comicFile)
