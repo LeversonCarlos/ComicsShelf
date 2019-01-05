911c169dc66d0b52efcce1fb1b53b39d3b454493
Leverson Carlos <lcjohnny@hotmail.com>
2019-01-05 11:23:09 -0200

    oops

--- ComicsShelf/Library/Service/ILibraryService.cs
+++ ComicsShelf/Library/Service/ILibraryService.cs
@@ -10,7 +10,7 @@ namespace ComicsShelf.Library
       Task<bool> Validate(Helpers.Database.Library library);
       Task<bool> AddLibrary(Helpers.Database.Library library);
       Task<List<Helpers.Database.ComicFile>> SearchFilesAsync(Helpers.Database.Library library);
-      Task ExtractCoverAsync(Helpers.Database.Library library, Helpers.Database.ComicFile comicFile);
+      Task ExtractCoverAsync(Helpers.Database.Library library, Helpers.Database.ComicFile comicFile, Action successCallback);
       Task ExtractPagesAsync(Helpers.Database.Library library, Views.File.FileData fileData);
    }
 }
