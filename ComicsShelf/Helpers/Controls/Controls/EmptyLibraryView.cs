6a8939f54ed69a4e9be5506e031393c68ee0745f
Leverson Carlos <lcjohnny@hotmail.com>
2019-01-26 14:32:44 -0200

    some more minor visual adjusts

--- ComicsShelf/Helpers/Controls/Controls/EmptyLibraryView.cs
+++ ComicsShelf/Helpers/Controls/Controls/EmptyLibraryView.cs
@@ -7,25 +7,29 @@ namespace ComicsShelf.Helpers.Controls
 
       public EmptyLibraryView()
       {
-         this.VerticalOptions = LayoutOptions.FillAndExpand;
-         this.Margin = new Thickness(10, 80);
+         this.VerticalOptions = LayoutOptions.CenterAndExpand;
+         this.Padding = new Thickness(20, 0, 20, 60);
 
          this.Children.Add(new Label {
-            Text = R.Strings.HOME_NO_COMIC_FILE_FOUND_ON_DEVICE_TITLE,
+            Text = R.Strings.LIBRARY_EMPTY_MESSAGE_TITLE,
             HorizontalOptions = LayoutOptions.Center,
             HorizontalTextAlignment = TextAlignment.Center,
+            VerticalOptions = LayoutOptions.Center, 
+            VerticalTextAlignment = TextAlignment.Center,
             FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
             FontAttributes = FontAttributes.Bold, 
-            TextColor = Colors.Accent
+            TextColor = Color.Accent
          });
 
          this.Children.Add(new Label
          {
-            Text = R.Strings.HOME_NO_COMIC_FILE_FOUND_ON_DEVICE_INSTRUCTIONS,
+            Text = R.Strings.HOME_LIBRARY_EMPTY_MESSAGE_INSTRUCTIONS,
             HorizontalOptions = LayoutOptions.Center,
             HorizontalTextAlignment = TextAlignment.Center,
+            VerticalOptions = LayoutOptions.Center,
+            VerticalTextAlignment = TextAlignment.Center,
             FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Label)),
-            TextColor = Colors.Accent
+            TextColor = Color.Accent
          });
 
       }
