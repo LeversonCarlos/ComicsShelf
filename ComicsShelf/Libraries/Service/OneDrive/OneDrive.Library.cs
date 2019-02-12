104dd001f08f209537bc876dec1f0950fb7bb215
Leverson Carlos <lcjohnny@hotmail.com>
2019-02-12 19:08:26 -0200

    folder select on one drive library add

--- ComicsShelf/Libraries/Service/OneDrive/OneDrive.Library.cs
+++ ComicsShelf/Libraries/Service/OneDrive/OneDrive.Library.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.OneDrive.Files;
 using Xamarin.OneDrive.Profile;
 
 namespace ComicsShelf.Libraries.Implementation
@@ -22,10 +25,34 @@ namespace ComicsShelf.Libraries.Implementation
          if (!await this.Connector.ConnectAsync()) { return false; }
          var profile = await this.Connector.GetProfileAsync();
          if (profile == null) { return false; }
+
+         var folder = await this.AddLibrary_FolderSelect(library);
+         if (folder == null) { return false; }
+         library.SetKeyValue("MainFolderID", folder.id);
+
          library.LibraryID = profile.id;
          library.Description = profile.Name;
          library.Available = true;
          return true;
+      }
+
+      private async Task<FileData> AddLibrary_FolderSelect(Library library)
+      {
+         var vm = new Libraries.Service.OneDrive.FolderSelector.SelectorVM();
+         vm.OnItemSelected += async (sender, item) =>
+         {
+            vm.IsBusy = true;
+            var folderChilds = await this.Connector.GetChildFoldersAsync(item);
+            vm.Data.ReplaceRange(folderChilds);
+            vm.CurrentItem = item;
+            vm.IsBusy = false;
+         };
+
+         var rootChilds = await this.Connector.GetChildFoldersAsync();
+         vm.Data.ReplaceRange(rootChilds);
+
+         var folder = await vm.OpenPage();
+         return folder;
       }
 
       public async Task<bool> RemoveLibrary(Library library)
