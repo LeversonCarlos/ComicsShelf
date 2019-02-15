d7958730fea00009cd325ae6b19a9e2b16649dc0
Leverson Carlos <lcjohnny@hotmail.com>
2019-02-15 14:59:03 -0200

    version 1.6

--- ComicsShelf.Android/Properties/AssemblyInfo.cs
+++ ComicsShelf.Android/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using Android.App;
+using System.Reflection;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -26,8 +25,8 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.34.0")]
-[assembly: AssemblyFileVersion("1.4.34.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
