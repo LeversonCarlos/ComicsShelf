fa5dd8e8d644de28cbdddd08c733231651ed71c9
Leverson Carlos <lcjohnny@hotmail.com>
2019-04-27 11:38:41 -0300

    library page

---
+++ ComicsShelf/Libraries/LibraryPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ComicsShelf.Libraries
+{
+   [XamlCompilation(XamlCompilationOptions.Compile)]
+   public partial class LibraryPage : ContentPage
+   {
+      public LibraryPage()
+      {
+         InitializeComponent();
+      }
+   }
+}
