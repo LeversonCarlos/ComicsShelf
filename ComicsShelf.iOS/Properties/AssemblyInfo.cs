81afe84acbfdfb676c0a4407b4dfa71e7378355e
Leverson Carlos <lcjohnny@hotmail.com>
2019-04-27 10:48:51 -0300

    manifest and some colors

--- ComicsShelf.iOS/Properties/AssemblyInfo.cs
+++ ComicsShelf.iOS/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ComicsShelf.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ComicsShelf.iOS")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Friendship Solutions")]
+[assembly: AssemblyProduct("Comics Shelf")]
+[assembly: AssemblyCopyright("Copyright © Friendship Solutions 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
