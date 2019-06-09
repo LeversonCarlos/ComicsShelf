3cc20adc528c847b0146f645e5af8610baa159cb
Leverson Carlos <lcjohnny@hotmail.com>
2019-06-09 18:20:21 -0300

    pages view

---
+++ ComicsShelf/ComicFiles/PagesView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ComicsShelf.ComicFiles
+{
+   [XamlCompilation(XamlCompilationOptions.Compile)]
+   public partial class PagesView : ContentPage
+   {
+      public PagesView()
+      {
+         InitializeComponent();
+      }
+   }
+}
