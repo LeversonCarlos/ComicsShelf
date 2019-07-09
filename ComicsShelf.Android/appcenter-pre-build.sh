4c8d4dad62921f20ca7663485d8df2f21c335be1
Leverson Carlos <lcjohnny@hotmail.com>
2019-07-09 08:25:30 -0300

    prebuild script

---
+++ ComicsShelf.Android/appcenter-pre-build.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+echo "APPLY PRODUCTION VALUES ON MANIFEST FILES"
+
+# VALIDATE
+if [ ! -n "$ComicsShelfApplicationID" ]
+then
+    echo "You need define the ComicsShelfApplicationID variable in App Center"
+    exit
+fi
+
+
+echo "versionBuild:"
+printf -v versionBuild "%03d" $APPCENTER_BUILD_ID
+echo $versionBuild
+
+
+# APPKEY FILE
+echo "APPKEY_FILE:"
+APPKEY_FILE=$APPCENTER_SOURCE_DIRECTORY/ComicsShelf/AppKeys.cs
+echo $APPKEY_FILE
+
+if [ -e "$APPKEY_FILE" ]
+then
+    sed -i '' 's/{YOUR_MICROSOFT_APPLICATION_ID}/'$ComicsShelfApplicationID'/' $APPKEY_FILE
+
+    echo "APPKEY_FILE CONTENT:"
+    cat $APPKEY_FILE
+fi
+
+
+# ANDROID MANIFEST FILE
+echo "ANDROID_MANIFEST_FILE:"
+ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/ComicsShelf.Android/Properties/AndroidManifest.xml
+echo $ANDROID_MANIFEST_FILE
+
+if [ -e "$ANDROID_MANIFEST_FILE" ]
+then
+    sed -i '' 's/{YOUR_MICROSOFT_APPLICATION_ID}/'$ComicsShelfApplicationID'/' $ANDROID_MANIFEST_FILE
+    sed -i '' 's/versionCode="[0-9.]*/&'$versionBuild'/' $ANDROID_MANIFEST_FILE
+    sed -i '' 's/versionName="[0-9.]*/&.'$versionBuild'/' $ANDROID_MANIFEST_FILE
+
+    echo "ANDROID_MANIFEST_FILE CONTENT:"
+    cat $ANDROID_MANIFEST_FILE
+fi
